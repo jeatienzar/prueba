059493a279ae011037ffaaf0cd39a7c4487c6e68
Jose Enrique Atienzar Ibanez <jeatienzar@gmail.com>
2015-10-27 18:48:30 +0100

    Initial Commit

---
+++ Prueba1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Prueba1
+//
+//  Created by José Enrique Atiénzar Ibáñez on 27/10/15.
+//  Copyright © 2015 José Enrique Atiénzar Ibáñez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
